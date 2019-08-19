823ee4de8648c7460cf8f76c579cb9c58564e06f
Stepan Krylov <stepan.krylov1991@yandex.ru>
2019-08-19 19:08:06 +0500

    finished_hash_md5.py

--- hash_md5.py
+++ hash_md5.py
@@ -1,6 +1,19 @@
 import json
 import hashlib
 
-with open(r'countries_wiki_url.txt', 'r', encoding='utf-8') as file:
-    p = [hashlib.md5(str(item).encode("utf-8")).hexdigest() for item in file]
-    print(len(p), p)
+#with open(r'countries_wiki_url.txt', 'r', encoding='utf-8') as file:
+#    p = [hashlib.md5(str(item).encode("utf-8")).hexdigest() for item in file]
+#    print(len(p), p)
+
+def hash():
+    with open(r'countries_wiki_url.txt', 'r', encoding='utf-8') as file:
+        for item in file:
+            yield hashlib.md5(str(item).encode("utf-8")).hexdigest()
+
+c = hash()
+
+p = []
+for item in c:
+    p.append(item)
+    
+print(len(p), p)
